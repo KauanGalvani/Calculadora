454f613f2de71535a8cdf43129c4758a5d711ef7
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-03-05 14:44:05 -0300

    adicionado a checagem de numeros validos

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -19,7 +19,7 @@ while (continuar == true)
 
     Console.WriteLine("Selecione uma opção valida: ");
 
-    string operacaoSelecionada = Console.ReadLine();
+    string? operacaoSelecionada = Console.ReadLine();
     if (operacaoSelecionada == "S" || operacaoSelecionada == "s")
     {
         continuar = false;
@@ -28,17 +28,26 @@ while (continuar == true)
 
     //leituras dos numeros digitados pelo usuario
     Console.Write("Digite o primeiro numero: ");
-    string strprimeiroNumero = Console.ReadLine();
+    string? strprimeiroNumero = Console.ReadLine();// (?) quando a variavel nn tem um valor definido.
 
     Console.Write("Digite o segundo numero: ");
-    string strsegundoNumero = Console.ReadLine();
+    string? strsegundoNumero = Console.ReadLine();
+
+    bool primeiroNumeroVazio = string.IsNullOrEmpty(strprimeiroNumero);
+    bool segundoNumeroVazio = string.IsNullOrEmpty(strsegundoNumero);
+
+    if (primeiroNumeroVazio == true || segundoNumeroVazio == true) 
+    {
+        Console.WriteLine("Dugite um numero valido!");
+        Console.Read();
+        continue;
+    }
 
     Console.WriteLine("-----------------------------------");
 
     Console.WriteLine();
 
     Console.WriteLine("O primeiro numero digitado foi: " + strprimeiroNumero);
-    Console.WriteLine();
     Console.WriteLine("O segundo numero digitado foi: " + strsegundoNumero);
 
     //conversao de strin para int
@@ -50,16 +59,22 @@ while (continuar == true)
     if (operacaoSelecionada == "1")
     {
         resultado = primeiroNumero + segundoNumero;
+        Console.WriteLine();
+        Console.WriteLine("A soma dos dois numeros resulta em: " + resultado);
     }
 
     else if (operacaoSelecionada == "2")
     {
         resultado = primeiroNumero - segundoNumero;
+        Console.WriteLine();
+        Console.WriteLine("A subtração dos dois numeros resulta em: " + resultado);
     }
 
     else if (operacaoSelecionada == "3")
     {
         resultado = primeiroNumero * segundoNumero;
+        Console.WriteLine();
+        Console.WriteLine("A multiplicação dos dois numeros resulta em: " + resultado);
     }
 
     else
@@ -70,12 +85,11 @@ while (continuar == true)
             return;
         }
         resultado = primeiroNumero / segundoNumero;
+        Console.WriteLine();
+        Console.WriteLine("A divisao dos dois numeros resulta em: " + resultado);
     }
 
-    Console.WriteLine();
-
-    Console.WriteLine("A soma dos dois numeros resulta em: " + resultado);
-
+    
     Console.ReadLine();
 }
 
